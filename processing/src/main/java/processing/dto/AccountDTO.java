8c2b614ea40830b3ac0601cb6974eb03b918200e
Vdym <vadimnaumnaum@gmail.com>
2024-02-19 22:11:24 +0100

    Bag fixes

--- processing/src/main/java/processing/dto/AccountDTO.java
+++ processing/src/main/java/processing/dto/AccountDTO.java
@@ -1,12 +1,15 @@
 package processing.dto;
 
 import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
-
+@Setter
+@Getter
 public class AccountDTO {
-    private Long userId;
-    private String currencyCode;
+    private Long user;
+    private String currency;
     private BigDecimal balance;
 
 }
--- processing/src/main/java/processing/service/AccountService.java
+++ processing/src/main/java/processing/service/AccountService.java
@@ -2,6 +2,7 @@ package processing.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import processing.convertor.AccountDtoConvertor;
 import processing.dto.AccountDTO;
 import processing.dto.NewAccountDTO;
@@ -17,7 +18,8 @@ public class AccountService {
     private final AccountRepository accountRepository;
     private final AccountDtoConvertor accountConvertor;
 
-    public AccountDTO createNewAccount(NewAccountDTO dto){
+    @Transactional
+    public AccountDTO createNewAccount(NewAccountDTO dto) {
         Account account = new Account();
         account.setBalance(new BigDecimal(0));
         account.setCurrencyCode(dto.getCurrencyCode());
