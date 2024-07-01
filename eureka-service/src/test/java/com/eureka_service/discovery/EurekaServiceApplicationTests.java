36d2f7e4dec1529fca1932c137fae7cd04d15dc9
Vdym <vadimnaumnaum@gmail.com>
2024-07-01 21:13:12 +0200

    Created eureka service spring cloud

---
+++ eureka-service/src/test/java/com/eureka_service/discovery/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka_service.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
